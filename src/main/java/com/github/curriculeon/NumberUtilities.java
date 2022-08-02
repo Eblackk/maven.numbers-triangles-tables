1dbce8b8d53dfae6842c3eccf04efd5606af3cc8
Eblackk <aps14blacerig@gmail.com>
2022-08-01 21:22:46 -0400

    GetRange Tests Passed

--- src/main/java/com/github/curriculeon/NumberUtilities.java
+++ src/main/java/com/github/curriculeon/NumberUtilities.java
@@ -3,6 +3,7 @@ package com.github.curriculeon;
 
 public class NumberUtilities {
     public static String getEvenNumbers(int start, int stop) {
+
         return null;
     }
 
@@ -16,19 +17,38 @@ public class NumberUtilities {
         return null;
     }
 
-    public static String getRange(int start) {
-        return null;
+    public static String getRange(int stop) {
+        // if i know the range stops at 10
+        // i know it will give me 0,1,2,3,4,5,6,7,8,9,10
+        String result = "";
+        int loopnumber = 0;
+        while (loopnumber < stop) {
+            result += loopnumber;
+            loopnumber = loopnumber + 1;
+        }
+        return result;
     }
 
     public static String getRange(int start, int stop) {
-        return null;
+        String result = "";
+        int loopnumber = start;
+        while (loopnumber < stop) {
+            result += loopnumber;
+            loopnumber = loopnumber + 1;
+        }
+        return result;
     }
 
 
     public static String getRange(int start, int stop, int step) {
-        return null;
+        String result = "";
+        int loopnumber = start;
+        while (loopnumber < stop) {
+            result += loopnumber;
+            loopnumber += step;
+        }
+        return result;
     }
-
 
     public static String getExponentiations(int start, int stop, int step, int exponent) {
         return null;
