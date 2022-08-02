f493149d00851512d5217cc2bc463a71e6341498
Eblackk <aps14blacerig@gmail.com>
2022-08-01 21:58:56 -0400

    getOddNumbers Passed

--- src/main/java/com/github/curriculeon/NumberUtilities.java
+++ src/main/java/com/github/curriculeon/NumberUtilities.java
@@ -3,13 +3,19 @@ package com.github.curriculeon;
 
 public class NumberUtilities {
     public static String getEvenNumbers(int start, int stop) {
-
-        return null;
+        
     }
 
 
     public static String getOddNumbers(int start, int stop) {
-        return null;
+        String result = "";
+        int loopnumber = start;
+        while (loopnumber < stop) {
+            result += loopnumber;
+            loopnumber += 2;
+        }
+        return result;
+
     }
 
 
