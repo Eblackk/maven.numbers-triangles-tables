1290623ce10d04888b0ec759546c58f05b136b9e
Eblackk <aps14blacerig@gmail.com>
2022-08-06 16:59:06 -0400

    updated Table Utils

--- src/main/java/com/github/curriculeon/TableUtilities.java
+++ src/main/java/com/github/curriculeon/TableUtilities.java
@@ -3,11 +3,18 @@ package com.github.curriculeon;
 public class TableUtilities {
     public static String getSmallMultiplicationTable() {
         String result = "";
-        int loopnumber = 1 ;
-        while (loopnumber < 6) {
-            result += loopnumber;
-            result += " | ";
-            loopnumber += 1;
+        int columnNumber = 1;
+        while (columnNumber < 6) {
+            int rowNumber = 1;
+
+            while (rowNumber < 6) {
+                result += rowNumber;
+                result += " | ";
+                rowNumber += 1;
+
+            }
+            result += "\n";
+            columnNumber += 1;
         }
 
 
